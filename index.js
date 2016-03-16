08197d380bfc9f0bc415ccd5493c52730076fb31
Alexander Sanchez <asanchez.dev@gmail.com>
2016-03-16 16:00:31 -0400

    adding mongo db support

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
 
-app.listen(80);
+var url = 'mongodb://localhost:27017/garage';
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+  db.close();
+});
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// returns a json
+app.get('/api/:garage_id/q/space', function (req, res) {
+  // look at the current instance of
+  res.send("something");
+});
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
