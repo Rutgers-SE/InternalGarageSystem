9707fb9c972c5a431b33d200bc162f2b185f3f1d
sajenesis <eyaansa@gmail.com>
2016-03-16 23:46:08 -0400

    greatest contribution ever

---
+++ device.js
@@ -0,0 +1,72 @@
+// Device
+
+function Device() {
+  this.deviceType = undefined;         // Used to determine the specific device type
+  this.signalType = "stream";          // default set to stream
+  this.name = "undersigned_device";    // Used to designate the specific device
+}
+
+// Static functions
+Device.on = function () {
+
+};
+
+Device.emit = function () {
+
+};
+
+Device.init = function () {
+
+};
+
+Device.terminate = function () {
+
+};
+
+// Dynamic functions:
+Device.prototype = {
+
+  on : function () {
+
+  },
+
+  emit : function () {
+
+  },
+
+  log : function () {
+
+  },
+
+  viewDeviceLog : function () {
+
+  }
+
+};
+
+// Device Types
+var camera = new Device({
+    deviceType : "camera",
+    signalType : "stream"
+
+});
+
+var sensor = new Device({
+    deviceType : "sensor",
+    signalType : "stream"
+});
+
+var entranceGate = new Device({
+    deviceType : "entranceGate",
+    signalType : "switch"
+});
+
+var terminal = new Device ({
+    deviceType : "terminal",
+    signalType : "stream"
+});
+
+var qrScanner = new Device({
+    deviceType : "qrScanner",
+    signalType : "stream"
+});
