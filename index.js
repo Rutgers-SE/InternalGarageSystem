67088f396407f6359ec8639b3889a4546c81f46c
Alexander Sanchez <asanchez.dev@gmail.com>
2016-04-03 03:09:57 -0400

    getting the structure for DO :tada:

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var _ = require('lodash');
-var spaceMan = require("./spaceManager.js");
+"use strict";
 var express = require("express");
+var _ = require('lodash');
+var spaceMan = require("./lib/spaceManager.js");
+var DeviceOrchestrator = require('./lib/DeviceOrchestrator');
 var app = express();
+var api = require("./lib/rest.js");
+var deviceViews = require('./lib/deviceViews');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert'),
-    url = 'mongodb://localhost:27017/garage';
-
 var spaceManOne = new spaceMan(200);
 
 app.use(express.static('public'));
@@ -16,125 +16,67 @@ server.listen(8080);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  // this should be the device panel
   res.render('index');
 });
 
-// This string is how we interface from the reservation system to the garage system.
-// notice how it starts with api
-// then :garage_id
-// then o. o mean object
-//      b means boolean
-//      n means number
-// -----vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv-------
+app.use('/api', api); // rest api...  look at line 3
+app.use('/devices', deviceViews); // rest api...  look at line 3
 
-//Check if garage is full
-//Should have seperate one for walkin and reservation
-app.get('/api/:garage_id/b/full', function (req, res) {
-  // look at the current instance of
-  res.json(false); // this should change.
-});
 
-//statistics
-app.get('/api/:garage_id/o/statistics', function (req, res) {
-  var garageId = req.params.garage_id;
-  res.json({
-    "thisshouldhavesomeinformation": "righthere",
-    "garage_id": garageId
+var devoc = new DeviceOrchestrator({io});
+
+io.on('connection', function (socket) {
+
+  devoc.on('dev:trigger', function (payload) {
+    this.emit("dev:response", "this is the response");
   });
-});
 
-var parseStartFinish = (req) => {
-  return {
-    start: new Date(req.params.start),
-    start: new Date(req.params.finish)
-  };
-};
+})
 
-
-
-//Check to see if reservation is available
-app.get('/api/:garage_id/b/checkreservation', function(req, res) {
-  var r = parseStartFinish(req);
-  res.json(spaceManOne.checkReservation(r.start, r.finish));
-});
-
-//Attempt to make reservation
-app.get('/api/:garage_id/b/setreservation', function(req, res) {
-  var r = parseStartFinish(req);
-  res.json(spaceManOne.setReservation(r.date1, r.date2));
-});
-
-//Get reserved spaces at specified timeslot
-app.get('/api/:garage_id/n/getreservedspaces', function(req, res){
-  res.json(spaceManOne.getReservedSpaces(new Date(req.params.date)));
-});
-
-//Get unreserved spaces at specified timeslot
-app.get('/api/:garage_id/n/getunreservedspaces/:y/:m/:d/:h', function(req, res){
-  res.json(spaceManOne.getUnreservedSpaces(new Date(req.params.date)));
-});
-
-//Get the currentTimeSlots array
-app.get('/api/:garage_id/o/getreservedtimes', function(req, res) {
-  // TODO: implement
-  res.json(spaceManOne.getReservedTimes());
-});
-
-function matchId(id) {
-  return  function (dev) {
-    if (id == dev.id) {
-      return true;
-    }
-  };
-}
-
-var registeredDevices = [];
-var idCounter = 0;
 
 //Socket.io
 // This is where the devices will be triggered in "REAL TIME"
-io.on('connection', function (socket) {
-  socket.on('dev:kill', function (data) {
-    var oldLength = registeredDevices.length;
-    registeredDevices = _.filter(registeredDevices, (devs) => {
-      var id = data.id;
-      if (devs.id == data.id) return false;
-      return true;
-    });
+//io.on('connection', function (socket) {
+  //socket.on('dev:kill', function (data) {
+    //var oldLength = registeredDevices.length;
+    //registeredDevices = _.filter(registeredDevices, (devs) => {
+      //var id = data.id;
+      //if (devs.id == data.id) return false;
+      //return true;
+    //});
 
-    if (oldLength != registeredDevices.length) {
-      socket.emit('dev:killed', {
-        deviceId: id
-      });
-    } else{
-      socket.emit('dev:killed', {
-        error: "Could not kill the device instance from the system."
-      });
-    }
+    //if (oldLength != registeredDevices.length) {
+      //socket.emit('dev:killed', {
+        //deviceId: id
+      //});
+    //} else{
+      //socket.emit('dev:killed', {
+        //error: "Could not kill the device instance from the system."
+      //});
+    //}
 
-  });
-  socket.on('dev:register', function (data) {
-    var id = idCounter += 1;
-    data.id = id;
-    data.registeredDeviceCount = registeredDevices.length;
-    registeredDevices.push(data);
-    socket.emit('dev:successfully-registered', data);
-  });
-
-
+  //});
+  //socket.on('dev:register', function (data) {
+    //var id = idCounter += 1;
+    //data.id = id;
+    //data.registeredDeviceCount = registeredDevices.length;
+    //registeredDevices.push(data);
+    //socket.emit('dev:successfully-registered', data);
+  //});
 
 
 
-  socket.on('dev:trigger', function (pl) {
-    var target = _.filter(registeredDevices, matchId(pl.id))
-      .each(function (dev) {
-
-      });
-  });
 
 
+  //socket.on('dev:trigger', function (pl) {
+    //var target = _.filter(registeredDevices, matchId(pl.id))
+      //.each(function (dev) {
 
-});
+      //});
+  //});
+
+
+
+//});
 
 console.log("Listening on port: 8080");
