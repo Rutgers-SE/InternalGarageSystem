c36769f1dd6e57d713b8f881edc720606c68d82f
Alexander Sanchez <asanchez.dev@gmail.com>
2016-04-28 00:52:05 -0400

    adding some fake data stuff

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
 "use strict";
+var _ = require('lodash');
 module.exports = function ({spaceManOne}) {
   var r = require("express").Router();
   // we need to use passport for OAuth2
 
+  function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
 
   var parseStartFinish = (req) => {
     return {
@@ -10,6 +14,30 @@ module.exports = function ({spaceManOne}) {
       finish: new Date(req.query.finish)
     }
   }
+
+  r.post('/fakeData', function (req, res) {
+
+    for (let _ = 0; _ < 400; _++) {
+      let start = randomDate(new Date(2016, 4, 30), new Date(2016, 6, 4))
+      let finish = randomDate(new Date(2016, 7, 1), new Date(2016, 12, 4))
+      let s = randomDate(new Date(2016, 7, 1), new Date(2016, 12, 4))
+      let f = randomDate(new Date(2016, 9, 10), new Date(2017, 2, 4))
+      console.log("start", start);
+      console.log("finish",finish);
+      spaceManOne.setReservation(start, finish);
+      spaceManOne.setReservation(s, f);
+    }
+
+    var times = spaceManOne.getReservedTimes();
+    var xs = _.range(0, times.length);
+    res.json(_.zip(xs, times));
+  });
+
+  r.get('/spaceData', function (req, res) {
+    var times = spaceManOne.reservedTimes
+    var xs = _.range(0, times.length);
+    res.json(_.zip(xs, times));
+  });
 
   r.get('/:garage_id/b/full', function (req, res) {
     // look at the current instance of
